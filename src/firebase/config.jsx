9927e517190abcfee891b18c330115d826bce9b6
Alisson Oliveira <binho_alisson@hotmail.com>
2023-06-21 23:26:00 -0300

    avançando nas primeiras configurações do projeto

---
+++ src/firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCq7HXXtsFd_C9swOJ0usQ0KW-T4zW5YsQ",
+  authDomain: "miniblog-78636.firebaseapp.com",
+  projectId: "miniblog-78636",
+  storageBucket: "miniblog-78636.appspot.com",
+  messagingSenderId: "302032838960",
+  appId: "1:302032838960:web:737a07022684b490e8e8f5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
